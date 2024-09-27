a1c2200e228e5c57451846dd15ebcb641deb8830
adamdev <adamdevofficial@protonmail.com>
2024-09-27 03:52:51 -0400

    Added even more members to CZFTSWeapon

--- src/entity/zwep_weapon.h
+++ src/entity/zwep_weapon.h
@@ -17,10 +17,42 @@ namespace zdb
 		void FireProjectile();
 		void Reload();
 	private:
-		const char* m_WeaponType;
+		const char* m_InternalName;
+		const char* m_DisplayName;
+		const char* m_Description;
 
+		const char* m_WeaponType;
+		
 		// magazines are put into CZAmmo instances, but for now i'll tack it in here
 		int m_RemainingAmmo;
 		int m_AmmoCount;
+		int m_NumMags;
+
+		float m_MuzzleVelocity;
+		float m_AccScalarMin;
+		float m_AccScalarMax;
+		float m_GravityAcceleration;
+
+		const char* m_ModelName;
+		const char* m_FireAnimName;
+		const char* m_HitAnimName;
+
+		float m_Encumberance;
+
+		bool m_BurstMode;
+		bool m_SingleMode;
+		bool m_AutoMode;
+
+		float m_Timer1;
+		float m_Timer2;
+
+		float m_RecoilPct;
+
+		float m_MaximumRange;
+		float m_EffectiveRange;
+
+		int m_SlotCost;
+
+		bool m_HasBackblast;
 	};
 }
