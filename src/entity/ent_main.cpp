984dd1e4ac5620d1ea711dc4b710c4745842b322
adamdev <adamdevofficial@protonmail.com>
2024-09-27 02:58:57 -0400

    Declarations of CZSealBody

---
+++ src/entity/ent_main.cpp
@@ -0,0 +1,89 @@
+#include "ent_main.h"
+
+namespace zdb
+{
+	CZSealBody::CZSealBody()
+	{
+		m_Position = { 0.0f, 0.0f, 0.0f };
+		m_Yaw = 0.0f;
+		m_WrittenPosition = { 0.0, 0.0, 0.0 };
+		m_WrittenYaw = 0.0f;
+
+		m_SkelRoot = new CZSealBodyBone("skel_root");
+		m_LFoot = new CZSealBodyBone("lfoot");
+		m_RFoot = new CZSealBodyBone("rfoot");
+		m_LHand = new CZSealBodyBone("lhand");
+		m_LHand = new CZSealBodyBone("rhand");
+		m_Hips = new CZSealBodyBone("hips");
+		m_Head = new CZSealBodyBone("head");
+		m_Neck = new CZSealBodyBone("neck");
+		m_SpineHi = new CZSealBodyBone("spinehi");
+		m_SpineLo = new CZSealBodyBone("spinelo");
+		m_LThigh = new CZSealBodyBone("lthigh");
+		m_RThigh = new CZSealBodyBone("rthigh");
+		m_LCalf = new CZSealBodyBone("lcalf");
+		m_RCalf = new CZSealBodyBone("rcalf");
+		m_LBicep = new CZSealBodyBone("lbicep");
+		m_RBicep = new CZSealBodyBone("rbicep");
+		m_LForearm = new CZSealBodyBone("lforearm");
+		m_RForearm = new CZSealBodyBone("rforearm");
+		m_LSCap = new CZSealBodyBone("lscap");
+		m_RSCap = new CZSealBodyBone("rscap");
+		m_LShoulderWgt = new CZSealBodyBone("lshoulder_wgt");
+		m_RShoulderWgt = new CZSealBodyBone("rshoulder_wgt");
+		m_AimNodes = new CZSealBodyBone("aimnodes");
+		m_LToe = new CZSealBodyBone("ltoe");
+		m_RToe = new CZSealBodyBone("rtoe");
+	}
+
+	CZSealBody::~CZSealBody()
+	{
+
+	}
+
+	void CZSealBody::SetPosition(float x, float y, float z)
+	{
+		m_WrittenPosition.x = x;
+		m_WrittenPosition.y = y;
+		m_WrittenPosition.z = z;
+		WritePosition();
+	}
+
+	void CZSealBody::SetPosition(zdb::Vector3 position)
+	{
+		SetPosition(position.x, position.y, position.z);
+	}
+
+	void CZSealBody::WritePosition()
+	{
+		m_Position.x = m_WrittenPosition.x;
+		m_Position.y = m_WrittenPosition.y;
+		m_Position.z = m_WrittenPosition.z;
+	}
+
+	zdb::Vector3 CZSealBody::GetPosition() const
+	{
+		return m_Position;
+	}
+
+	float CZSealBody::GetYaw() const
+	{
+		return m_Yaw;
+	}
+
+	CZSealBodyBone::CZSealBodyBone()
+	{
+		m_BoneName = "";
+		m_BoneWorldPosition = { 0.0, 0.0, 0.0 };
+		m_BoneLocalPosition = { 0.0, 0.0, 0.0 };
+		m_BoneRotation = { 0.0, 0.0, 0.0 };
+	}
+
+	CZSealBodyBone::CZSealBodyBone(const char* name)
+	{
+		m_BoneName = name;
+		m_BoneWorldPosition = { 0.0, 0.0, 0.0 };
+		m_BoneLocalPosition = { 0.0, 0.0, 0.0 };
+		m_BoneRotation = { 0.0, 0.0, 0.0 };
+	}
+}
--- src/entity/ent_main.h
+++ src/entity/ent_main.h
@@ -1,7 +1,77 @@
 #pragma once
+#include "node/node_main.h"
+#include "math/zmath_main.h"
+
 namespace zdb
 {
 	class CEntity
+	{
+	public:
+		CEntity();
+	};
+
+	class CZSealBody : CEntity
+	{
+	public:
+		CZSealBody();
+		~CZSealBody();
+
+		void SetPosition(float x, float y, float z);
+		void SetPosition(zdb::Vector3 position);
+		void WritePosition();
+
+		zdb::Vector3 GetPosition() const;
+		float GetYaw() const;
+
+		bool AggroLookAt(int param_1, void* param_2, zdb::Vector3 lookPosition);
+	private:
+		zdb::Vector3 m_Position;
+		float m_Yaw;
+		zdb::Vector3 m_Scale;
+
+		zdb::Vector3 m_WrittenPosition;
+		float m_WrittenYaw;
+
+		CZSealBodyBone* m_SkelRoot;
+		CZSealBodyBone* m_Hips;
+		CZSealBodyBone* m_SpineLo;
+		CZSealBodyBone* m_SpineHi;
+		CZSealBodyBone* m_Neck;
+		CZSealBodyBone* m_LBicep;
+		CZSealBodyBone* m_RBicep;
+		CZSealBodyBone* m_LThigh;
+		CZSealBodyBone* m_RThigh;
+		CZSealBodyBone* m_LCalf;
+		CZSealBodyBone* m_RCalf;
+		CZSealBodyBone* m_LFoot;
+		CZSealBodyBone* m_RFoot;
+		CZSealBodyBone* m_LToe;
+		CZSealBodyBone* m_RToe;
+		CZSealBodyBone* m_LHand;
+		CZSealBodyBone* m_RHand;
+		CZSealBodyBone* m_LForearm;
+		CZSealBodyBone* m_RForearm;
+		CZSealBodyBone* m_LSCap;
+		CZSealBodyBone* m_RSCap;
+		CZSealBodyBone* m_LShoulderWgt;
+		CZSealBodyBone* m_RShoulderWgt;
+		CZSealBodyBone* m_Head;
+		CZSealBodyBone* m_AimNodes;
+	};
+
+	class CZSealBodyBone : CNodeEx
+	{
+	public:
+		CZSealBodyBone();
+		CZSealBodyBone(const char* name);
+	private:
+		const char* m_BoneName;
+		zdb::Vector3 m_BoneWorldPosition;
+		zdb::Vector3 m_BoneLocalPosition;
+		zdb::Vector3 m_BoneRotation;
+	};
+
+	class CZSealEx : public CNodeEx
 	{
 
 	};
