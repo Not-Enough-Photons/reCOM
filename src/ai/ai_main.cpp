47d630eeaa59c5332a1f2f3b63bad94cf20b0cd1
adamdev <adamdevofficial@protonmail.com>
2024-10-06 15:49:22 -0400

    Added debug AI state function

--- src/ai/ai_main.cpp
+++ src/ai/ai_main.cpp
@@ -1,4 +1,19 @@
 #include <cstdint>
+#include <iostream>
+
+enum AIState
+{
+	STATE_SLEEP,
+	STATE_IDLE,
+	STATE_HOLD,
+	STATE_PATH,
+	STATE_CREEP,
+	STATE_SLIDE,
+	STATE_FOLLOW,
+	STATE_CLIMB,
+	STATE_TEST,
+	NUM_STATES
+};
 
 typedef struct AIParams
 {
@@ -24,6 +39,66 @@ typedef struct AISensors
 int SetParameter(long param_2, char* paramName, uint32_t* ptr, int32_t param_4)
 {
 	return 1;
+}
+
+unsigned int GetDebugAIState(long param_1)
+{
+	int iVar1;
+	const char* name;
+	int state;
+
+	while (true)
+	{
+		if (param_1 == 0 || (8 < state))
+		{
+			return 1;
+		}
+
+		if ((state & 255) < 10)
+		{
+			switch (state)
+			{
+			case STATE_SLEEP:
+				name = "sleep";
+				break;
+			case STATE_IDLE:
+				name = "idle";
+				break;
+			case STATE_HOLD:
+				name = "hold";
+				break;
+			case STATE_PATH:
+				name = "path";
+				break;
+			case STATE_CREEP:
+				name = "creep";
+				break;
+			case STATE_SLIDE:
+				name = "slide";
+				break;
+			case STATE_FOLLOW:
+				name = "follow";
+				break;
+			case STATE_CLIMB:
+				name = "climb";
+				break;
+			case STATE_TEST:
+				name = "test";
+				break;
+			default:
+				name = "error";
+			}
+		}
+
+		if (strcmp((char*)param_1, name))
+		{
+			break;
+		}
+
+		state++;
+	}
+
+	return state & 255;
 }
 
 void SetAIParameters(uint32_t* aiPtr, long param_2)
