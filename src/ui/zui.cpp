9451506ffa6dfd938239f805ee08860dd5c1ac8b
adamdev <adamdevofficial@protonmail.com>
2024-10-06 16:12:22 -0400

    UI descriptions for commands

--- src/ui/zui.cpp
+++ src/ui/zui.cpp
@@ -2,6 +2,83 @@
 
 namespace zdb
 {
+	const char* CZUI::GetCommandDescription(CommandDescType desc)
+	{
+		const char* description;
+
+		switch (desc)
+		{
+		case DESC_BASE:
+			description = "CMD_ACTION";
+			break;
+		case DESC_THROW_ITEM:
+			description = "Throw or place item at crosshairs . . .";
+			break;
+		case DESC_FIRE_AT_WILL:
+			description = "Take any and every open shot";
+			break;
+		case DESC_FOLLOW:
+			description = "Follow me";
+			break;
+		case DESC_HOLD_POSITION:
+			description = "Stay where you are";
+			break;
+		case DESC_HOLD_FIRE:
+			description = "Shoot only when shot at";
+			break;
+		case DESC_REGROUP:
+			description = "Regroup at team leader's position";
+			break;
+		case DESC_HOSTAGE_FOLLOW:
+			description = "Person under reticule will follow";
+			break;
+		case DESC_RUN_TO:
+			description = "Run to . . .";
+			break;
+		case DESC_MOVE_TO:
+			description = "Move out to . . .";
+			break;
+		case DESC_STEALTH_TO:
+			description = "Using shadows and coverage, move to . . .";
+			break;
+		case DESC_ATTACK_TO:
+			description = "Take all available shots and move to . . .";
+			break;
+		case DESC_ESCORT_TO:
+			description = "Escort me to . . .";
+			break;
+		case DESC_COVER_TARGET:
+			description = "Covering fire at my crosshairs";
+			break;
+		case DESC_LASER:
+			description = "Laser designate guided weapon target . . .";
+			break;
+		case DESC_BREACH:
+			description = "BREACH";
+			break;
+		case DESC_GET_DOWN:
+			description = "Get down on the ground";
+			break;
+		case DESC_PULL_OUT:
+			description = "PULLOUT";
+			break;
+		case DESC_OVERWATCH:
+			description = "Assume overwatch position";
+			break;
+		case DESC_EXTRACT_HOSTAGE:
+			description = "Extract escort or hostage";
+			break;
+		case DESC_REMOVE_KILLS:
+			description = "Remove any hostiles";
+			break;
+		default:
+			// description = FUN_002ca4a0();
+			break;
+		}
+
+		return description;
+	}
+
 	void CZUI::SendUIMessage(float time, const char* message, char* messagePtr, void* param_4, void* param_5, int param_6)
 	{
 		C2DMessageString str;
--- src/ui/zui_main.h
+++ src/ui/zui_main.h
@@ -1,6 +1,31 @@
 #pragma once
 namespace zdb
 {
+	enum CommandDescType
+	{
+		DESC_BASE,
+		DESC_THROW_ITEM,
+		DESC_FIRE_AT_WILL,
+		DESC_FOLLOW,
+		DESC_HOLD_POSITION,
+		DESC_HOLD_FIRE,
+		DESC_REGROUP,
+		DESC_HOSTAGE_FOLLOW,
+		DESC_RUN_TO,
+		DESC_MOVE_TO,
+		DESC_STEALTH_TO,
+		DESC_ATTACK_TO,
+		DESC_ESCORT_TO,
+		DESC_COVER_TARGET,
+		DESC_LASER,
+		DESC_BREACH,
+		DESC_GET_DOWN,
+		DESC_PULL_OUT,
+		DESC_OVERWATCH,
+		DESC_EXTRACT_HOSTAGE,
+		DESC_REMOVE_KILLS
+	};
+
 	class C2D
 	{
 
@@ -19,6 +44,7 @@ namespace zdb
 	class CZUI
 	{
 	public:
+		static const char* GetCommandDescription(CommandDescType desc);
 		static void SendUIMessage(float param_1, const char* message, char* messagePtr, void* param_4, void* param_5, int param_6);
 	};
 }
