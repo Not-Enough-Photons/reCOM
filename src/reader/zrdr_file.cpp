8ac2c3ca6c1a42a7abc0de691fa0a2228152ebb1
adamdev <adamdevofficial@protonmail.com>
2024-10-10 20:53:48 -0400

    Reader hash function

--- src/reader/zrdr_file.cpp
+++ src/reader/zrdr_file.cpp
@@ -1,24 +1,88 @@
-#include <istream>
-#include <cstddef>
+#include "zrdr_parse.h"
 
 namespace zdb
 {
-	class CZRdrFile
-	{
-	private:
-		float_t m_SoundDistanceClose;
-		float_t m_SoundDistanceMed;
-		float_t m_SoundDistanceFar;
+    static int GetReaderHashDifference(unsigned char parentByte, unsigned char childByte)
+    {
+        int parentOffset = static_cast<int>(parentByte);
+        int childOffset = static_cast<int>(childByte);
 
-		// ZAMMO
-		char m_InternalName[256];
-		char m_DisplayName[256];
-		char m_Description[2048];
-		int32_t m_ID;
-		int32_t m_ImpactDamage;
-		int32_t m_Stun;
-		int32_t m_Piercing;
-		float_t m_Explosion_Damage;
-		float_t m_Explosion_Radius;
-	};
+        if ((ZAR_HASH_TABLE[parentByte] & 1) != 0)
+        {
+            parentOffset = parentByte + 32;
+        }
+
+        if ((ZAR_HASH_TABLE[childByte] & 1) != 0)
+        {
+            childOffset = childByte + 32;
+        }
+
+        return childOffset - parentOffset;
+    }
+
+    static int GetNextReader(const char* parentReader, const char* childReader)
+    {
+        char parentChar = *parentReader;
+        char childChar = *childReader;
+
+        if (parentChar != 0)
+        {
+            int parentValue = static_cast<int>(parentChar + 32);
+            int childValue = static_cast<int>(childChar + 32);
+
+            if ((ZAR_HASH_TABLE[parentChar] & 1) == 0)
+            {
+                parentValue = static_cast<int>(parentChar);
+            }
+
+            if ((ZAR_HASH_TABLE[childChar] & 1) == 0)
+            {
+                childValue = static_cast<int>(childChar);
+            }
+
+            if (parentValue != childValue)
+            {
+                return GetEntryHashDifference(*parentReader, *childReader);
+            }
+
+            parentReader++;
+
+            unsigned char parentByte = 0;
+            unsigned char childByte = 0;
+
+            while (true)
+            {
+                childReader++;
+
+                parentByte = *parentReader;
+
+                if (parentByte == 0)
+                {
+                    break;
+                }
+
+                childByte = *childReader;
+                parentValue = parentByte;
+                if ((ZAR_HASH_TABLE[parentByte] & 1) != 0)
+                {
+                    parentValue = parentByte + 32;
+                }
+
+                childValue = childByte + 32;
+                if ((ZAR_HASH_TABLE[childByte] & 1) == 0)
+                {
+                    childValue = childByte;
+                }
+
+                if (parentValue != childValue)
+                {
+                    break;
+                }
+
+                parentReader++;
+            }
+        }
+
+        return GetEntryHashDifference(*parentReader, *childReader);
+    }
 }
---
+++ src/zgame.cpp
@@ -0,0 +1,6 @@
+#include "zgame.h"
+
+namespace zdb
+{
+
+}
