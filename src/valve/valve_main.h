46445e3b1a0502cd9e89e5f8f681b0d9ee1a62c3
adamdev <adamdevofficial@protonmail.com>
2024-10-15 04:30:11 -0400

    Update valve_main.h

--- src/valve/valve_main.h
+++ src/valve/valve_main.h
@@ -1,4 +1,10 @@
 #pragma once
+/// <summary>
+/// Determines the pooling type for the valve.
+/// <para> ValveType::PERM - Lasts the entire lifetime of the level. Will pool if value reaches threshold. </para>
+/// <para> ValveType::TEMP - Loads once then unloads when done. </para>
+/// <para> ValveType::PERSIST - Lasts the entire lifetime of the game, and will pool if the value reaches threshold.</para>
+/// </summary>
 enum ValveType
 {
 	PERM,
