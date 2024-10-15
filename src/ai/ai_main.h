ad7b266ede0ccb1783e6d18619142a6eb5bbf8e7
adamdev <adamdevofficial@protonmail.com>
2024-10-15 19:05:32 -0400

    AI state declarations

--- src/ai/ai_main.h
+++ src/ai/ai_main.h
@@ -1,74 +1,201 @@
 #pragma once
-namespace zdb
+enum FireTeam
 {
-	enum FireTeam
-	{
-		FT_UNKNOWN,
-		FIRETEAM,
-		ABLE,
-		BRAVO,
-		ESCORTEE,
-		FRIENDLY,
-		NEUTRAL,
-		TURRETS,
-		SUPPORT,
-		HOSTAGE,
-		ENEMIES,
-		ALLIES
-	};
+	FT_UNKNOWN,
+	FIRETEAM,
+	ABLE,
+	BRAVO,
+	ESCORTEE,
+	FRIENDLY,
+	NEUTRAL,
+	TURRETS,
+	SUPPORT,
+	HOSTAGE,
+	ENEMIES,
+	ALLIES
+};
 
-	enum SealCommand
-	{
-		CMD_UNKNOWN,
-		CMD_ACTION,
-		CMD_DEPLOY,
-		CMD_FIREATWILL,
-		CMD_FOLLOW,
-		CMD_HOLDPOS,
-		CMD_HOLDFIRE,
-		CMD_REGROUP,
-		CMD_ESCORT,
-		CMD_RUNTO,
-		CMD_MOVETO,
-		CMD_STEALTHTO,
-		CMD_ATTACKTO,
-		CMD_LEADTO,
-		CMD_COVERTARGET,
-		CMD_LASETARGET,
-		CMD_BREACH,
-		CMD_GETDOWN,
-		CMD_PULLOUT,
-		CMD_OVERWATCH,
-		CMD_SUPPORT,
-		CMD_EXTRACT,
-		CMD_CLEARAREA,
-		CMD_RESTRAIN,
-		CMD_REMOVEKILLS,
-		CMD_AWAKE,
-		CMD_SLEEP,
-		CMD_AMBUSH
-	};
+enum SealCommand
+{
+	CMD_UNKNOWN,
+	CMD_ACTION,
+	CMD_DEPLOY,
+	CMD_FIREATWILL,
+	CMD_FOLLOW,
+	CMD_HOLDPOS,
+	CMD_HOLDFIRE,
+	CMD_REGROUP,
+	CMD_ESCORT,
+	CMD_RUNTO,
+	CMD_MOVETO,
+	CMD_STEALTHTO,
+	CMD_ATTACKTO,
+	CMD_LEADTO,
+	CMD_COVERTARGET,
+	CMD_LASETARGET,
+	CMD_BREACH,
+	CMD_GETDOWN,
+	CMD_PULLOUT,
+	CMD_OVERWATCH,
+	CMD_SUPPORT,
+	CMD_EXTRACT,
+	CMD_CLEARAREA,
+	CMD_RESTRAIN,
+	CMD_REMOVEKILLS,
+	CMD_AWAKE,
+	CMD_SLEEP,
+	CMD_AMBUSH
+};
 
-	class CAiPlanner
-	{
-	public:
-		virtual void Act();
-		virtual void Stop();
-		virtual float GetPlanCost();
-	};
+class CAiState
+{
 
-	class CBasicPlanner : public CAiPlanner
-	{
+};
 
-	};
+class CAiSHold : public CAiState
+{
 
-	class CStealthPlanner : public CAiPlanner
-	{
+};
 
-	};
+class CAiSAvoid : public CAiState
+{
 
-	class CRestrainedPlanner : public CAiPlanner
-	{
+};
 
-	};
-}
+class CAiSDeploy : public CAiState
+{
+
+};
+
+class CAiSSurrender : public CAiState
+{
+
+};
+
+class CAiSUseVehicle : public CAiState
+{
+
+};
+
+class CAiSGoto : public CAiState
+{
+
+};
+
+class CAiSFollow : public CAiState
+{
+
+};
+
+class CAiSBreach : public CAiState
+{
+
+};
+
+class CAiSAction : public CAiState
+{
+
+};
+
+class CAiSPursue : public CAiState
+{
+
+};
+
+class CAiSAnimate : public CAiState
+{
+
+};
+
+class CAiSCQBAttack : public CAiState
+{
+
+};
+
+class CAiSFlee : public CAiState
+{
+
+};
+
+class CAiSPathRecover : public CAiState
+{
+
+};
+
+class CAiSRescued : public CAiState
+{
+
+};
+
+class CAiSSuspended : public CAiState
+{
+
+};
+
+class CAiSShotAt : public CAiState
+{
+
+};
+
+class CAiSNoAmmo : public CAiState
+{
+
+};
+
+class CAiSHostage : public CAiState
+{
+
+};
+
+class CAiSPlan : public CAiState
+{
+
+};
+
+class CAiSEvent : public CAiState
+{
+
+};
+
+class CAiSTarget : public CAiState
+{
+
+};
+
+class CAiSGrenade : public CAiState
+{
+
+};
+
+class CAiSStunResponse : public CAiState
+{
+
+};
+
+class CAiSRush : public CAiState
+{
+
+};
+
+class CAiPlanner
+{
+public:
+	virtual void Act();
+	virtual void Stop();
+	virtual float GetPlanCost();
+};
+
+class CBasicPlanner : public CAiPlanner
+{
+
+};
+
+class CStealthPlanner : public CAiPlanner
+{
+
+};
+
+class CRestrainedPlanner : public CAiPlanner
+{
+
+};
