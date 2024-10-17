727c3398d6629fb35257cb88baf59e6d72c8d926
adamdev <adamdevofficial@protonmail.com>
2024-10-16 21:20:55 -0400

    CWorld now moved and changed

--- src/node/node_world.h
+++ src/node/node_world.h
@@ -3,8 +3,38 @@
 
 namespace zdb
 {
-	class CWorld : public CNode
+	class CWorld
 	{
+	public:
+		static int GetVersion();
 
+		void Init();
+		void Uninit();
+		void diTick();
+		void Update();
+
+		void AddChild(const CNode& child);
+		void AddLandmark(const CNode& landmark);
+		void AddTextureAssetCharacter(const CNode& textureAsset);
+
+		void DeleteChildren();
+		void DeleteLandmark(const CNode& landmark);
+
+		void ClearLightMapList();
+		void ClearShadowList();
+		void WipeoutTextureAssetCharacters();
+
+		void ComputeLightIntensity(float intensity, const CPnt3D& point, float* lightRef);
+
+		void GetTexHandle() const;
+		undefined4 GetTextureByName(const char* name) const;
+		int GetModel() const;
+	};
+
+	class CWind
+	{
+	public:
+		static void RegisterAnimCommands();
+		void CmdParseWind();
 	};
 }
