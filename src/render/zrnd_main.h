494d04137c9e9dadadd625259f6f2b19fa067d47
adamdev <adamdevofficial@protonmail.com>
2024-10-21 03:26:45 -0400

    Update zrnd_main.h

--- src/render/zrnd_main.h
+++ src/render/zrnd_main.h
@@ -1,8 +1,21 @@
 #pragma once
 void zRndrInit();
 
+namespace zdb
+{
+	typedef struct tag_ZVIS_FOV
+	{
+
+	};
+}
+
 class CPipe
 {
 public:
 	static void Init();
+
+	void RenderNode(zdb::CNode* node, zdb::tag_ZVIS_FOV* fovTag);
+	void RenderUINode(zdb::CNode* node);
+	void RenderUINodeRecursive(zdb::CNode* node);
+	void RenderWorld(zdb::CWorld* world);
 };
