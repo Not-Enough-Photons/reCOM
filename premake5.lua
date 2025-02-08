fb296921fec5a5eebda7ce87ef90fd268d2f568f
adamdev <adamdevofficial@protonmail.com>
2025-02-08 03:13:14 -0500

    Improved premake5 building system

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,13 @@
 workspace "reCOM"
     configurations { "Debug", "Release" }
     platforms { "x86", "x86_64", "PS2" }
+	defaultplatform "x86"
 
-project "game"
+project "fts"
     kind "ConsoleApp"
     language "C++"
+
+	flags { "MultiProcessorCompile" }
 
     includedirs {
 		"vendor",
@@ -12,6 +15,8 @@ project "game"
 	}
 	
     libdirs { "lib" }
+	
+	links { "opengl32.lib", "SDL3.lib", "SDL3_image.lib", "glew32.lib" }
 
     files { 
         "src/Apps/FTS/**.cpp",
@@ -21,25 +26,29 @@ project "game"
         "vendor/**.cpp",
 		"vendor/**.h",
     }
-	
-	
 
     filter { "configurations:Debug" }
         defines { "DEBUG" }
         symbols "On"
+		targetdir "output/debug"
 
     filter { "configurations:Release" }
         defines { "NDEBUG" }
         optimize "On"
+		targetdir "output/release"
 
     filter { "platforms:x86" }
         system "Windows"
         architecture "x86"
+		targetname ("fts32")
 
     filter { "platforms:x86_64" }
         system "Windows"
         architecture "x86_64"
+		targetname ("fts64")
 
     filter { "platforms:PS2" }
         system "bsd"
         architecture "mips"
+		targetname ("SCUS-972")
+		targetextension (".05")
