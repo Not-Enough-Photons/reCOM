f55754e133987c3faa7cc579290d2ebbd89dde7a
adamdev <adamdevofficial@protonmail.com>
2025-02-27 14:46:45 -0500

    zReader load custom strings exercise

--- src/Apps/FTS/st_test.cpp
+++ src/Apps/FTS/st_test.cpp
@@ -22,6 +22,7 @@ extern const char* dir;
 
 CLetterBox* letterbox;
 CZPauseTest* pausemenu;
+C2DString strings[8];
 
 bool LoadWorld(const char* db);
 
@@ -51,15 +52,43 @@ bool CTestState::Init()
     letterbox->AddLetterbox();
     pausemenu = new CZPauseTest();
     pausemenu->ReInit();
+
+    auto reader = zrdr_read("./data/zrdr/test_text.rdr", NULL, 0);
+
+    for (u32 i = 1; reader->array->integer; i++)
+    {
+        auto node = &reader->array[i];
+        PNT3D color;
+        PNT2D position;
+        
+        char* text = zrdr_findstring(node, "text");
+        zrdr_findPNT3D(node, "color", &color);
+        zrdr_findPNT2D(node, "position", &position);
+
+        strings[i].Load(text, NULL, 250, 250 * i - 0.25f);
+        strings[i].m_color.x = color.x;
+        strings[i].m_color.y = color.y;
+        strings[i].m_color.z = color.z;
+    }
+    
+    zrdr_free(reader);
     return true;
 }
 
 void CTestState::Tick(f32 dT)
 {
+    SDL_Event e;
+    SDL_PollEvent(&e);
     SDL_SetRenderDrawColor(theWindow->GetRenderer(), 15, 15, 15, 255);
     SDL_RenderClear(theWindow->GetRenderer());
-    letterbox->Tick(dT);
-    letterbox->Draw(NULL);
-    pausemenu->Draw(NULL);
+    // letterbox->Tick(dT);
+    // letterbox->Draw(NULL);
+    // pausemenu->Draw(NULL);
+
+    for (u32 i = 0; i < 2; i++)
+    {
+        strings[i].Draw(NULL);
+    }
+    
     SDL_RenderPresent(theWindow->GetRenderer());
 }
